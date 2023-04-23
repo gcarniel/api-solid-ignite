f46f8cc35f9624087026bb9b11682b9c41f7d606
Gabriel Carniel <gpcarniel@outlook.com>
2023-04-23 12:42:50 -0300

    hash de senha do usuario

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,7 @@
 import { prisma } from '@/lib/prisma'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
+import { hash } from 'bcryptjs'
 
 export async function register(req: FastifyRequest, rep: FastifyReply) {
   const userSchema = z.object({
@@ -11,8 +12,16 @@ export async function register(req: FastifyRequest, rep: FastifyReply) {
 
   const { email, name, password } = userSchema.parse(req.body)
 
+  const passwordHash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({ where: { email } })
+
+  if (userWithSameEmail) {
+    return rep.status(409).send()
+  }
+
   await prisma.user.create({
-    data: { email, name, password_hash: password },
+    data: { email, name, password_hash: passwordHash },
   })
 
   rep.status(201).send()
