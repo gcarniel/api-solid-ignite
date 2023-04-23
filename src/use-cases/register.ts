adae53dbbdcd05807afb734324c1d9002f011563
Gabriel Carniel <gpcarniel@outlook.com>
2023-04-23 14:16:00 -0300

    criando interface para repositorio de users e metodo findByEmail

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,5 @@
 import { hash } from 'bcryptjs'
-import { prisma } from '@/lib/prisma'
+import { UsersRepository } from '@/repositories/users-repository'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -8,12 +8,12 @@ interface RegisterUseCaseRequest {
 }
 
 export class RegisterUseCase {
-  constructor(private usersRepository: any) {}
+  constructor(private usersRepository: UsersRepository) {}
 
   async execute({ email, name, password }: RegisterUseCaseRequest) {
     const passwordHash = await hash(password, 6)
 
-    const userWithSameEmail = await prisma.user.findUnique({ where: { email } })
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
       throw new Error('Email already exist')
