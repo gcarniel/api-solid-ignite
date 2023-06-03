9fa45aa5ef701fd5835cc30994e3e4e3c0a2c372
Gabriel Carniel <gpcarniel@outlook.com>
2023-06-03 19:00:10 -0300

    testes de checkins criados

---
+++ src/http/controllers/gyms/search.spec.ts
@@ -0,0 +1,54 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Search Gyms (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able search gyms by title', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'JS Gym',
+        latitude: -22.1253749,
+        longitude: -51.3613116,
+        description: 'teste',
+        phone: '11111111111',
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'TS Gym',
+        latitude: -22.1253749,
+        longitude: -51.3613116,
+        description: 'teste',
+        phone: '11111111111',
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/search')
+      .query({
+        q: 'JS',
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({ title: 'JS Gym' }),
+    ])
+  })
+})
