d49440be3266af7840412d2fd6ed663076094d16
Gabriel Carniel <gpcarniel@outlook.com>
2023-06-03 22:38:30 -0300

    refresh token e testes

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -25,7 +25,25 @@ export async function authenticate(req: FastifyRequest, rep: FastifyReply) {
       },
     )
 
-    return rep.status(200).send({ token })
+    const refreshToken = await rep.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: '7d',
+        },
+      },
+    )
+
+    return rep
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({ token })
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return rep.status(400).send({ message: err.message })
